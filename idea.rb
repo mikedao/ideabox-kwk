1131b820a0cb512fe89e794249d00fdad07a3021
Michael Dao <mike@turing.io>
2018-05-16 14:11:23 -0600

    I4 Complete

--- idea.rb
+++ idea.rb
@@ -6,6 +6,23 @@ class Idea
     @description = description
   end
 
+  def self.update(id, data)
+    database.transaction do
+      database['ideas'][id] = data
+    end
+  end
+
+  def self.find(id)
+    raw_idea = find_raw_idea(id)
+    Idea.new(raw_idea[:title], raw_idea[:description])
+  end
+  
+  def self.find_raw_idea(id)
+    database.transaction do
+      database['ideas'].at(id)
+    end
+  end
+
   def self.delete(position)
     database.transaction do
       database['ideas'].delete_at(position)
